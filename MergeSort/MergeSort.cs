6b8e884b11dac5c777102baba54750ea2cc3bd3d
Leo927 <asd347925290@gmail.com>
2020-08-03 21:11:42 -0600

    Add project files.

---
+++ MergeSort/MergeSort.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MergeSort
+{
+    class MergeSort
+    {
+        public static List<IComparable> Sort(List<IComparable> source)
+        {
+            return SortRecur(source);
+        }
+
+        private static List<IComparable> SortRecur(List<IComparable> source)
+        {
+            if (source.Count <= 1)
+                return source;
+            var left = SortRecur(source.Take(source.Count / 2).ToList());
+            var right = SortRecur(source.Skip(source.Count / 2).Take(source.Count).ToList());
+            return Merge(left, right);
+        }
+
+        public static List<IComparable> Merge(List<IComparable> lhs, List<IComparable> rhs)
+        {
+            int lhsIndex = 0, rhsIndex = 0;
+            List<IComparable> result = new List<IComparable>();
+            while (lhsIndex <= lhs.Count || rhsIndex <= rhs.Count)
+            {
+                IComparable left = lhs[lhsIndex];
+                IComparable right = rhs[rhsIndex];
+                if (lhs[lhsIndex].CompareTo(rhs[rhsIndex]) <= 0)
+                {
+                    result.Add(lhs[lhsIndex]);
+                    lhsIndex++;
+                }
+                else
+                {
+                    result.Add(rhs[rhsIndex]);
+                    rhsIndex++;
+                }
+                if (lhsIndex >= lhs.Count)
+                {
+                    result.AddRange(rhs.Skip(rhsIndex).Take(rhs.Count));
+                    return result;
+                }
+                if (rhsIndex >= rhs.Count)
+                {
+                    result.AddRange(lhs.Skip(lhsIndex).Take(lhs.Count));
+                    return result;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ MergeSort/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rng = new Random();
+            Console.WriteLine("please enter the amount of number to generate");
+            int count = Convert.ToInt32( Console.ReadLine());
+            List<IComparable> unsortedList = new List<IComparable>(count);
+            Console.WriteLine("The following is the sorted list");
+            for (int i = 0; i < count; i++)
+            {
+                unsortedList.Add(rng.Next());
+            }
+            MergeSort.Sort(unsortedList)
+                .ForEach(x => Console.WriteLine(x));
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
